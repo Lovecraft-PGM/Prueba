169a1fa4ac9d640c1da1619a02ad229558c7409f
Santiago <herrerafernandezsantiago@gmail.com>
2023-05-25 14:30:54 -0500

    "First test of develop"

--- serializado.py
+++ serializado.py
@@ -14,10 +14,3 @@ import pickle
 
 
 
-archivo = input('Por favor ingrese el nombre del archivo que quiere leer:')
-
-miarchivo = open (archivo+'.txt','rb')
-lista  = pickle.load(miarchivo)
-miarchivo.close()
-
-print(lista)
